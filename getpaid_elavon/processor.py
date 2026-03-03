22ba3a7113d17605c1eeec69353b4e98d7c95abf
Adam Przyborowski <adam.przyborowski@ordergroup.pl>
2026-03-03 10:43:35 +0100

    Feature DK-1256 update new version

--- getpaid_elavon/processor.py
+++ getpaid_elavon/processor.py
@@ -87,7 +87,6 @@ class ElavonProcessor(BaseProcessor):
         )
 
         self.payment.external_id = session_resp.get("id")
-        self.payment.save(update_fields=["external_id"])
 
         payment_hpp_url = session_resp.get("url")
 
